ba601a9ff9f17c7cab88dbcf85b4f1ee1d13dbf0
Shanmugam Elangovan <shanmugamcsk8@gmail.com>
2023-01-25 00:06:45 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule,} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StudentDetailsComponent } from './student-details/student-details.component';
+const routes: Routes = [
+ {path : 'dashboard',component: DashboardComponent},
+ {path : 'student-details',component: StudentDetailsComponent}
+
+  
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    StudentDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,MatFormFieldModule,MatInputModule,
+    MatSidenavModule,MatListModule,RouterModule.forRoot(routes),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
